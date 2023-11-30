bb9a7ee25763a829044c06dfc952b29c3713dbd8
im410726 <o1.mirsaitov@gmail.com>
2023-11-30 10:37:18 +0600

    Your commit message

---
+++ src/app/shared/foo/foo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foo',
+  templateUrl: './foo.component.html',
+  standalone: true,
+  styleUrls: ['./foo.component.css']
+})
+export class FooComponent {
+
+}
